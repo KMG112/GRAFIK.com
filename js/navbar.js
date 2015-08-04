06eaf92f470e7e9af3d09db4037964e5d2694735
KMG112 <mattwuertz@gmail.com>
2015-08-04 10:42:21 -0400

    removed ngfx

---
+++ js/navbar.js
@@ -0,0 +1,69 @@
+var app = angular.module("app", ["ngAnimate"]);
+
+app.controller("NavbarController", function(){
+	this.isExpanded = false;
+	this.expandHamburger = function(){
+		this.isExpanded = true;
+	};
+	this.collapsHamburger = function(){
+		this.isExpanded = false;
+	}
+})
+
+app.directive("expandMe", function($animate){
+	return function(scope, element, attrs){
+		scope.$watch(attrs.expandMe, function(newVal){
+			if (newVal) {
+			$animate.addClass(element, "expand");
+		}else {
+                $animate.removeClass(element, "expand");
+            }
+		});
+	}
+})
+
+app.animation(".expand", function(){
+
+	return {
+		    addClass: function(element, className){
+
+		    		TweenMax.to(element, 1, {marginTop: "1px", marginBottom: "1px"});
+				
+			},
+            removeClass: function(element, className) {
+            TweenMax.to(element, 1, {y:-20});
+        }
+		}
+})
+
+// var app = angular.module("app", ["ngAnimate"]);
+
+// app.controller("AppCtrl", function() {
+//     this.isHidden = false;
+//     this.fadeIt = function() {
+//         this.isHidden = !this.isHidden;
+//     };
+// });
+
+// app.directive("hideMe", function($animate) {
+//     return function(scope, element, attrs) {
+//         scope.$watch(attrs.hideMe, function(newVal) {
+//             if (newVal) {
+//                 $animate.addClass(element, "fade");
+//             } else {
+//                 $animate.removeClass(element, "fade");
+//             }
+//         });
+//     };
+// });
+
+// app.animation(".fade", function() {
+//     return {
+//         addClass: function(element, className) {
+//             TweenMax.to(element, 1, {opacity: 0});
+//         },
+//         removeClass: function(element, className) {
+//             TweenMax.to(element, 1, {opacity: 1});
+//         }
+//     };
+// });
