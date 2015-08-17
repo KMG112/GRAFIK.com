094ce6f26c5426c9b3bfc3cc6b5d571f7e9ba225
KMG112 <mattwuertz@gmail.com>
2015-08-17 15:54:24 -0400

    sliders work and inputs work

--- js/app.js
+++ js/app.js
@@ -1,41 +1,64 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation({
-
+	slider:{
+    on_change: function(){
+    	if($("#slider2 input").val() ==0){
+    		$(".dot-labels span").css({"color": "#555555"})
+    		$(".dot-holder span").show()
+    	    $(".month-label-now").css({"color":"white"});
+    	    $("#dot-1").css({"display":"none"});
+    	}
+    	else if($("#slider2 input").val() ==1){
+    		$(".dot-labels span").css({"color": "#555555"})
+    		$(".dot-holder span").show()
+    	    $(".month-label-6").css({"color":"white"});
+    	    $("#dot-2").css({"display":"none"});
+    	}
+    	else if($("#slider2 input").val() ==2){
+    		$(".dot-labels span").css({"color": "#555555"})
+    		$(".dot-holder span").show()
+    	    $(".month-label-9").css({"color":"white"});
+    	    $("#dot-3").css({"display":"none"});
+    	}
+    	else if($("#slider2 input").val() ==3){
+    		$(".dot-labels span").css({"color": "#555555"})
+    		$(".dot-holder span").show()
+    	    $(".month-label-year").css({"color":"white"});
+    	    $("#dot-4").css({"display":"none"});
+    	 }  
+    	
+    }
+  }
 	});
 
-
-// form slider js
-$(".range-slider-handle").mousemove(function(){
-	$("#current-number-selected-budget").text($('.range-slider input').val())
-})
-$("#current-number-selected-budget").text($('.range-slider input').val())
-
-
-$(".tabs .active h2").css("color", "white");
+// $(".tabs .active h2").css("color", "white");
 
 $("#grafik-form").on("click", function(){
-
 	setInterval(function(){
-		$(".tabs a[tabindex='0'] h1").css("color", "white");
-		$(".tabs a[tabindex='-1'] h1").css("color", "black")}, 10)
-		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+		$(".tabs a[tabindex='0'] h1").css({"color": "white"});
+		$(".tabs a[tabindex='-1'] h1").css({"color": "#555555"});
+		$(".tabs a[tabindex='0'] span").css({"color": "#ff6600"});
+		$(".tabs a[tabindex='-1'] span").css({"color": "#555555"});
+	});
 });
 
-var radio1_labels = $(".radio-toolbar label");
-var radio2_labels = $(".radio-toolbar-2 label");
-var radio3_labels = $(".radio-toolbar-3 label");
-
 function toggle_visibility(el, next) {
-	$.each(el, function(){
-		$(this).on("click", function() {
-			$(next).show();
-			$("html, body").animate({ scrollTop: $(document).height()-$(window).height()}, "slow");
-		});
+	el.on("click", function() {
+		$(next).show();
+		$("html, body").animate({ scrollTop: $(document).height()-$(window).height()}, "easeOutQuint");
+		$(window).trigger("resize");
 	});
 }
-toggle_visibility(radio1_labels, ".radio-toolbar-2")
-toggle_visibility(radio2_labels, "#info-enter")
+toggle_visibility($(".radio-toolbar label"), ".radio-toolbar-2");
+toggle_visibility($(".radio-toolbar-2 label"), "#info-enter");
+toggle_visibility($(".radio-toolbar-3 label"), "#info-enter-panel-2");
 
 
-toggle_visibility(radio3_labels, "#info-enter-panel-2")
+$(document).on("ready", function() {
+	$(window).trigger("resize");
+});
+
+
+
+
